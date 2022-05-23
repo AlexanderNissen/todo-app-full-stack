c00575d28c3a74cff395d553c2dd3547429da25b
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-05-23 22:31:16 +0200

    S4S07 - Implementing routing for components using React Router framework

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 import LoginComponent from './LoginComponent';
 import './LoginComponent.css';
+import WelcomeComponent from './WelcomeComponent';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 class TodoApp extends Component {
   render() {
-    return(
+    return (
       <div className="TodoApp">
-        My Todo Application
-        <LoginComponent />
+        <Router>
+          <Routes>
+            <Route path="/" element={<LoginComponent/>}/>
+            <Route path="/login" element={<LoginComponent />}/>
+            <Route path="/welcome" element={<WelcomeComponent />}/>
+          </Routes>
+        </Router>
       </div>
     );
   }
---
+++ src/components/todo/WelcomeComponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class WelcomeComponent extends Component {
+  render() {
+    return <div>Welcome USERNAME</div>
+  }
+}
+
+
+export default WelcomeComponent;
