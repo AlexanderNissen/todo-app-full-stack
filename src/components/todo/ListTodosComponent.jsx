7bc1d8e9bb5a5835f5b617d2a9cee82127a404c2
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-05-25 22:15:23 +0200

    S4S11 - Create a basic React list Todo component

---
+++ src/components/todo/ListTodosComponent.jsx
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+
+class ListTodosComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        {id: 1, description: 'Learn to computer architecture'},
+        {id: 2, description: 'Learn React'},
+        {id: 3, description: 'Learn Spring Boot'}
+      ]
+    }
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>List Todos</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.state.todos.map (
+              todo =>  
+                <tr>
+                  <td>{todo.id}</td>
+                  <td>{todo.description}</td>
+                </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </div>
+    );
+  }
+}
+
+export default ListTodosComponent;
--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import LoginComponent from './LoginComponent.jsx';
 import './LoginComponent.css';
 import WelcomeComponent from './WelcomeComponent.jsx';
+import ListTodosComponent from './ListTodosComponent.jsx';
 import withParams from './WithParams.jsx';
 import withNavigation from './WithNavigation.jsx';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -18,6 +19,7 @@ class TodoApp extends Component {
               <Route path="/" element={<LoginComponentWithNavigation/>}/>
               <Route path="/login" element={<LoginComponentWithNavigation/>}/>
               <Route path="/welcome/:name" element={<WelcomeComponentWithParams/>}/>
+              <Route path="/todos" element={<ListTodosComponent/>}/>
               <Route path="*" element={<ErrorComponent/>}/>
           </Routes>
         </Router>
