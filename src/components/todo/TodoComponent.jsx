2166bc415d365c737a7544b32780619a9f208e11
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-08-23 22:25:02 +0200

    S6S48 - Add prototype of Todo-form with moment, Formik and Bootstrap

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import moment from 'moment'
+import { Formik, Form, Field } from 'formik';
 
 class TodoComponent extends Component {
 
@@ -6,12 +8,36 @@ class TodoComponent extends Component {
         super(props);
 
         this.state = {
-            id: this.props.params.id
+            id: this.props.params.id,
+            description: 'Learn Forms',
+            targetDate: moment(new Date()).format('YYYY-MM-DD')
         }
     }
     render() {
         return (
-            <div>Todo component for id {this.props.params.id}</div>
+            <div>
+                <h1>Todo</h1>
+                <div className="container">
+                    <Formik>
+                    {/* Define method in Formik component that takes props as inputs and returns html of the form*/} 
+                        {
+                            (props) => (
+                                <Form>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description"/>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target Date</label>
+                                        <Field className="form-control" type="date" name="targetDate"/>
+                                    </fieldset>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+
+                </div>
+            </div>
         )
     }
 }
