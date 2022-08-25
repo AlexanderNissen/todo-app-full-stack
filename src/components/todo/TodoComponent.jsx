8ee278e587181c7876ed9d077ace168b3fe136bb
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-08-25 22:27:59 +0200

    S6S49 - Initializing values and handling form submits with Formik

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import moment from 'moment'
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field } from 'formik'
+import '../../bootstrap.css'
 
 class TodoComponent extends Component {
 
@@ -9,16 +10,49 @@ class TodoComponent extends Component {
 
         this.state = {
             id: this.props.params.id,
-            description: 'Learn Forms',
+            description: 'Learn Forms now',
             targetDate: moment(new Date()).format('YYYY-MM-DD')
         }
+
+        this.onSubmit = this.onSubmit.bind(this)
     }
+
+    onSubmit(values) {
+        console.log(values)
+    }
+
     render() {
+        {
+        /*
+        In JS, we can do:
+        let testList = {a: '1', b: '2', c: '3'}
+        let {a, b, c} = testList
+        */
+        }
+        let {description, targetDate} = this.state
+
         return (
             <div>
                 <h1>Todo</h1>
                 <div className="container">
-                    <Formik>
+                    {/*
+                    Below assignment of variables in Formik component is equal to:
+                    initialValues = {{
+                        description: description,
+                        targetDate: targetDate
+                    }}
+
+                    JS infers values when a property name identical to the variable name is passed. 
+                    */}
+                    <Formik 
+                        initialValues = {{
+                            description,
+                            targetDate
+                        }}
+                        onSubmit = {this.onSubmit}
+                    >
+                        
+
                     {/* Define method in Formik component that takes props as inputs and returns html of the form*/} 
                         {
                             (props) => (
@@ -31,6 +65,7 @@ class TodoComponent extends Component {
                                         <label>Target Date</label>
                                         <Field className="form-control" type="date" name="targetDate"/>
                                     </fieldset>
+                                    <button className="btn btn-success" type="submit">Save</button>
                                 </Form>
                             )
                         }
